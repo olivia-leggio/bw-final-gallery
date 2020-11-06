7b66c12b7203dfe4ba04986c27b5c480b3911d8f
Olivia Leggio <o.r.legg12@gmail.com>
2020-11-06 17:04:26 -0500

    first image set, javascript show full image

---
+++ static/main.js
@@ -0,0 +1,4 @@
+function showFull(num) {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("overImg").setAttribute("src", `static/img/large/${num}.jpg`)
+}
