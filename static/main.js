7b193f8ec3c83cb05cdaba34f4bcf2ef8635807c
Olivia Leggio <o.r.legg12@gmail.com>
2020-11-07 13:02:40 -0500

    js gallery working, added more images

--- static/main.js
+++ static/main.js
@@ -1,4 +1,34 @@
+/*
+shows overlay with full sized image*/
 function showFull(num) {
+    if (parseInt(num) <= 0) {
+        num = "13";
+    }
+    if (parseInt(num) > 13) {
+        num = "1";
+    }
     document.getElementById("overlay").style.display = "block";
-    document.getElementById("overImg").setAttribute("src", `static/img/large/${num}.jpg`)
+    var image = document.getElementById("overImg");
+    image.setAttribute("src", `static/img/large/${num}.jpg`);
+    image.setAttribute("data-num", num);
+
+    document.getElementById("progTxt").innerHTML = `${num} of 13`;
+    document.getElementById("set1").style.display = "none";
+}
+
+/*
+changes which full image is displayed*/
+function changeSlide(mod) {
+    var currStr = document.getElementById("overImg").getAttribute("data-num");
+    var currInt = parseInt(currStr);
+
+    var newInt = currInt + mod;
+    showFull(newInt);
+}
+
+/*
+hides the overlay and returns to the main gallery*/
+function closeSlides() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("set1").style.display = "block";
 }
